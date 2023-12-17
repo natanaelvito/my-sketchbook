66ac84f07af578554bd64fc1e19f62ef79b2258f
natanaelvito <121381231+natanaelvito@users.noreply.github.com>
2023-12-17 23:37:07 +0700

    Commit number ?

--- main.js
+++ main.js
@@ -7,19 +7,20 @@ const paper1 = document.querySelector("#p1");
 const paper2 = document.querySelector("#p2");
 const paper3 = document.querySelector("#p3");
 const paper4 = document.querySelector("#p4");
+const paper5 = document.querySelector("#p5");
+const paper6 = document.querySelector("#p6");
 
 // Event Listener
 prevBtn.addEventListener("click", goPrevPage);
 nextBtn.addEventListener("click", goNextPage);
 
-// I wanna make the pages loopable, meaning I could go from the first cover page to the back cover page back to back, I'll figure out but in the meantime, committing this bullshit
+// Loopable 
 
 // Arrow Keys Navigation
 
-
 // Business Logic
 let currentLocation = 1;
-let numOfPapers = 4;
+let numOfPapers = 6;
 let maxLocation = numOfPapers + 1;
 
 function openBook() {
@@ -58,6 +59,14 @@ function goNextPage() {
             case 4:
                 paper4.classList.add("flipped");
                 paper4.style.zIndex = 4;
+                break;
+            case 5:
+                paper5.classList.add("flipped");
+                paper5.style.zIndex = 5;
+                break;
+            case 6:
+                paper6.classList.add("flipped");
+                paper6.style.zIndex = 6;
                 closeBook(false);
                 break;
             default:
@@ -73,20 +82,28 @@ function goPrevPage() {
             case 2:
                 closeBook(true);
                 paper1.classList.remove("flipped");
-                paper1.style.zIndex = 4;
+                paper1.style.zIndex = 6;
                 break;
             case 3:
                 paper2.classList.remove("flipped");
-                paper2.style.zIndex = 3;
+                paper2.style.zIndex = 5;
                 break;
             case 4:
                 paper3.classList.remove("flipped");
-                paper3.style.zIndex = 2;
+                paper3.style.zIndex = 4;
                 break;
             case 5:
-                openBook();
                 paper4.classList.remove("flipped");
-                paper4.style.zIndex = 1;
+                paper4.style.zIndex = 3;
+                break;
+            case 6:
+                paper5.classList.remove("flipped");
+                paper5.style.zIndex = 2;
+                break;
+            case 7:
+                openBook();
+                paper6.classList.remove("flipped");
+                paper6.style.zIndex = 1;
                 break;
             default:
                 throw new Error("unknown state");
